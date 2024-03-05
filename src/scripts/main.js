64f2674fcf7c30133854d1c211a5d755b3000356
Matheus Leão <mleaopita@outlook.com>
2024-03-05 20:14:40 -0300

    criando contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
-console.log("teste");
+const dataDoEvento = new Date("Mar 28, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteOEvento % diaEmMs) / minutoEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteOEvento % diaEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaHoras);
+        document.getElementById('contador').innerHTML = 'Evento encerrado';
+    }
+}, 1000);
